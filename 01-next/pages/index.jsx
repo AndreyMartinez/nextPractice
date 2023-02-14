ceba403eea6146b9dea4585f0ed0bb86587609c1
AndreyMartinez <raphaelandrey.martinezvelasquez@mercadolibre.com.co>
2023-02-13 20:38:16 -0600

    add complements react

---
+++ 01-next/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+function HomePage() {
+  return (
+    <>
+    <Head>
+      <title>Hello Rapha</title>
+    </Head>
+      <div>Welcome to Next.js!</div>
+    </>
+  )
+}
+
+export default HomePage
