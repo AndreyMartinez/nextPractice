68d36b8a69f1286bf0de5ebec0427a00b0f0d882
AndreyMartinez <raphaelandrey.martinezvelasquez@mercadolibre.com.co>
2024-06-06 20:48:30 -0600

    add documentation

--- 02-next-pokemon-practice/pages/index.tsx
+++ 02-next-pokemon-practice/pages/index.tsx
@@ -1,9 +1,14 @@
+// Docuementación 01.MD
+import { Button, NextUIProvider } from "@nextui-org/react"
 interface Props {
 
 }
 const indexPage = ({ }: Props): JSX.Element => {
   return (
+    <NextUIProvider>
     <p>Hello world</p>
+    <Button color="gradient">hello</Button>
+    </NextUIProvider>
   )
 }
 
